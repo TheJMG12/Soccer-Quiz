1d57e04e57e707996c78b51e3e020a31d3d3c7c2
TheJMG12 <ngosine83@gmail.com>
2017-01-12 15:09:10 -0500

    Create soccerquiz.py

---
+++ soccerquiz.py
@@ -0,0 +1,208 @@
+#TRIVIA SOCCER!!!!
+
+print("This quiz will test your knowledge about soccer!!")
+
+name = input("input your name: ")
+
+print('\nHello there ' , name + 'Are you ready to take this quiz?\n')
+
+print('I will ask you 10 questions and give you 3 choices for each question')
+
+print('Select choice right choice by pressing A, B, or C')
+
+print("--------------------------------------------------------------------")
+
+#set quiz score to 0
+
+score = 0
+score = int(score) #convert 0 to number so we can add scores
+
+#question 1
+print("QUESTION 1: Who scored the winning goal in the 2010 World Cup Final?")
+print("A. Mario Gotze")
+print("B. Andres Iniesta")
+print("C. David Villa")
+print('')
+
+Q1answer = 'B' #correct answer
+Q1response = input('Your answer ')
+
+if Q1response =='B'or Q1response =='b':
+    print('Yes! '+ Q1response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 2
+print("QUESTION 2: Who won the Premier League in the 2015/2016 season?")
+print("A. Leicester City")
+print("B. Manchester City")
+print("C. Arsenal")
+print('')
+
+Q2answer = 'A' #correct answer
+Q2response = input('Your answer ')
+
+if Q2response =='A'or Q1response =='a':
+    print('Yes! '+ Q2response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 3
+print("QUESTION 3: How many times has Lionel Messi won the Ballon d’or?")
+print("A. 5")
+print("B. 6")
+print("C. 4")
+print('')
+
+Q3answer = 'A' #correct answer
+Q3response = input('Your answer ')
+
+if Q3response =='A'or Q3response =='a':
+    print('Yes! '+ Q3response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 4
+print("QUESTION 4: How many times has Manchester United won the English League Title")
+print("A. 17")
+print("B. 20")
+print("C. 18")
+print('')
+
+Q4answer = 'B' #correct answer
+Q4response = input('Your answer ')
+
+if Q4response =='B'or Q4response =='b':
+    print('Yes! '+ Q4response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 5
+print("QUESTION 5: What were the most amount of goals scored by a player one calendar year and by whom?")
+print("A. 91 - Messi")
+print("B. 85 - Muller")
+print("C. 107 - Ronaldo")
+print('')
+
+Q5answer = 'A' #correct answer
+Q5response = input('Your answer ')
+
+if Q5response =='A'or Q5response =='a':
+    print('Yes! '+ Q5response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 6
+print("QUESTION 6: Which country has the most World Cups?")
+print("A. Germany")
+print("B. Italy")
+print("C. Brazil")
+print('')
+
+Q6answer = 'C' #correct answer
+Q6response = input('Your answer ')
+
+if Q6response =='C'or Q6response =='c':
+    print('Yes! '+ Q6response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 7
+print("QUESTION 7: Who is the most expensive tranfer in history?")
+print("A. Paul Pogba")
+print("B. Gareth Bale")
+print("C. Carlos Tevez")
+print('')
+
+Q7answer = 'A' #correct answer
+Q7response = input('Your answer ')
+
+if Q7response =='A'or Q7response =='a':
+    print('Yes! '+ Q7response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 8
+print("QUESTION 8: Who has the most Champions League Titles?")
+print("A. Real Madrid")
+print("B. Barcelona")
+print("C. AC Milan")
+print('')
+
+Q8answer = 'A' #correct answer
+Q8response = input('Your answer ')
+
+if Q8response =='A'or Q8response =='a':
+    print('Yes! '+ Q8response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 9
+print("QUESTION 9: When was the first ever World Cup")
+print("A. 1954")
+print("B. 1900")
+print("C. 1930")
+print('')
+
+Q9answer = 'C' #correct answer
+Q9response = input('Your answer ')
+
+if Q9response =='C'or Q9response =='c':
+    print('Yes! '+ Q9response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 10
+print("QUESTION 10: What country does Bayern Munich play in")
+print("A. Netherlands")
+print("B. Germany")
+print("C. Belgium")
+print('')
+
+Q10answer = 'B' #correct answer
+Q10response = input('Your answer ')
+
+if Q10response =='B'or Q10response =='b':
+    print('Yes! '+ Q10response + ' is correct!')
+    score = score +1
+
+else:
+    print("Sorry you are wrong")
+
+print('Your current score is ' + str(score) + ' out of 10')
